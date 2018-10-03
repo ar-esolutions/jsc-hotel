d9765795055eb4162feca3e8ed1ac7d239d22b34
Martin Pianello <martin.pianello@claro.com.ar>
2018-10-03 11:15:19 -0300

    * Refactor Guest para que acepte null

---
+++ src/main/java/com/esolutions/trainings/jsc2/web/GuestResponse.java
@@ -0,0 +1,17 @@
+package com.esolutions.trainings.jsc2.web;
+
+public class GuestResponse {
+    private Integer guest;
+
+    public Integer getGuest() {
+        return guest;
+    }
+
+    public GuestResponse(Integer guest) {
+        this.guest = guest;
+    }
+
+    public void setGuest(Integer guest) {
+        this.guest = guest;
+    }
+}
--- src/main/java/com/esolutions/trainings/jsc2/web/RoomMapperController.java
+++ src/main/java/com/esolutions/trainings/jsc2/web/RoomMapperController.java
@@ -1,6 +1,5 @@
 package com.esolutions.trainings.jsc2.web;
 
-import com.esolutions.trainings.jsc2.model.Guest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoomMapperController {
 
     @GetMapping(value = "/floors/{floor}/rooms/{room}")
-    public Guest getGuestNumber(@PathVariable int floor, @PathVariable int room ){
-        return new Guest(0);
+    public GuestResponse getGuestNumber(@PathVariable int floor, @PathVariable int room ){
+        return new GuestResponse(null);
     }
 }
