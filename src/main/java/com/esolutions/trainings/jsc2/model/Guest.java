16881215c1e448217fcd649b07592683d075e6ce
mmezi <gonzalo.ariza@esolutions.com.ar>
2018-10-03 10:51:14 -0300

    cambiado script bd para que sea tabla de huespedes en vez de jugadores

---
+++ src/main/java/com/esolutions/trainings/jsc2/model/Guest.java
@@ -0,0 +1,17 @@
+package com.esolutions.trainings.jsc2.model;
+
+public class Guest {
+    private int guest;
+
+    public int getGuest() {
+        return guest;
+    }
+
+    public Guest(int guest) {
+        this.guest = guest;
+    }
+
+    public void setGuest(int guest) {
+        this.guest = guest;
+    }
+}
